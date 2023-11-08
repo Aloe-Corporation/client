1f4085922b3565090c89c984294c2dbcdbdb4ee6
Florian Richard <florian.richard@aloe-corp.com>
2023-11-08 15:37:24 +0100

    Add comments in client.go

--- client.go
+++ client.go
@@ -8,11 +8,15 @@ var (
 	instanciatedClient = make(map[string]*http.Client)
 )
 
+// Conf for the client. All parameters are required.
 type Conf struct {
-	URL          string `yaml:"url"`
-	PingEndpoint string `yaml:"ping_endpoint"`
+	URL          string `yaml:"url"`           // Base url of the target HTTP server such as https://myserver.com
+	PingEndpoint string `yaml:"ping_endpoint"` // Path of the ping endpoint of the target HTTP server
 }
 
+// ProxyFactoryHttpClient creates a new client if it does not exists in
+// `instanciatedClient` map. If the client key is already defined, the
+// function returns the associated client.
 func ProxyFactoryHttpClient(key string) *http.Client {
 	if c, ok := instanciatedClient[key]; ok {
 		return c
@@ -24,6 +28,7 @@ func ProxyFactoryHttpClient(key string) *http.Client {
 	return c
 }
 
+// FactoryHttpClient returns a fresh new http.Client instance.
 func FactoryHttpClient() *http.Client {
 	return &http.Client{}
 }

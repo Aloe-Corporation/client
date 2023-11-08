aafcc5ea11be58c23543564d91f4ce9a878c4c28
Florian Richard <florian.richard@aloe-corp.com>
2023-11-08 15:32:25 +0100

    Add comments in test/http_server_mock.go

--- test/http_server_mock.go
+++ test/http_server_mock.go
@@ -5,6 +5,8 @@ import (
 	"net/http/httptest"
 )
 
+// GetPingEndpoint is a HTTP mock endpoint used for testing.
+// The ping url is "/".
 func GetPingEndpoint() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" || r.Method != "GET" {
@@ -16,6 +18,8 @@ func GetPingEndpoint() *httptest.Server {
 	}))
 }
 
+// GetEndpoint is a HTTP mock endpoint to simulate a GET request that responds with data.
+// The only valid path is "/get".
 func GetEndpoint() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/get" || r.Method != "GET" {
@@ -28,6 +32,8 @@ func GetEndpoint() *httptest.Server {
 	}))
 }
 
+// GetEndpointWithoutBody is a HTTP mock endpoint to simulate a GET request that responds without data.
+// The only valid path is "/get".
 func GetEndpointWithoutBody() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/get" || r.Method != "GET" {
@@ -39,6 +45,10 @@ func GetEndpointWithoutBody() *httptest.Server {
 	}))
 }
 
+// GetEndpointWithHeader is a HTTP mock endpoint to simulate a GET request that await for
+// "test-header" header in the request. The server will respond with a 404 status code
+// if the incoming request do not contains the header.
+// The only valid path is "/get".
 func GetEndpointWithHeader() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/get" || r.Method != "GET" {
@@ -57,6 +67,8 @@ func GetEndpointWithHeader() *httptest.Server {
 	}))
 }
 
+// PostEndpoint is a HTTP mock endpoint to simulate a POST request that responds without data.
+// The only valid path is "/post".
 func PostEndpoint() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/post" || r.Method != "POST" {
@@ -69,6 +81,8 @@ func PostEndpoint() *httptest.Server {
 	}))
 }
 
+// PutEndpoint is a HTTP mock endpoint to simulate a PUT request that responds without data.
+// The only valid path is "/put".
 func PutEndpoint() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/put" || r.Method != "PUT" {
@@ -81,6 +95,8 @@ func PutEndpoint() *httptest.Server {
 	}))
 }
 
+// DeleteEndpoint is a HTTP mock endpoint to simulate a DELETE request that responds without data.
+// The only valid path is "/delete".
 func DeleteEndpoint() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/delete" || r.Method != "DELETE" {
